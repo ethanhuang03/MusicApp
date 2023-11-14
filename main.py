a09df8b3f84a9a8960cf41be0f3f980e2122bd6a
ethanhuang03 <ethanhuang301@gmail.com>
2023-11-14 23:03:12 +1300

    copied real time fft from somewhere

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+from realtimefft.stream_analyzer import Stream_Analyzer
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ear = Stream_Analyzer(
+    device=2,  # Pyaudio (portaudio) device index, defaults to first mic input
+    rate=None,  # Audio samplerate, None uses the default source settings
+    FFT_window_size_ms=80,  # Window size used for the FFT transform
+    updates_per_second=2000,  # How often to read the audio stream for new data
+    smoothing_length_ms=50,  # Apply some temporal smoothing to reduce noisy features
+    n_frequency_bins=600,  # The FFT features are grouped in bins
+    visualize=1,  # Visualize the FFT features with PyGame
+    verbose=0 # Print running statistics (latency, fps, ...)
+)
 
+fps = 60
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+last_update = time.time()
+while True:
+    if (time.time() - last_update) > (1./fps):
+        last_update = time.time()
+        raw_fftx, raw_fft, binned_fftx, binned_fft = ear.get_audio_features()
